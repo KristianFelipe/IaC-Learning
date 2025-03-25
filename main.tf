26b121bf560a3dcf64590e185eefc77d69cf9bf4
KristianFelipe <kristianlindgren3@gmail.com>
2025-03-25 09:39:57 +0100

    Testing av NSG

--- main.tf
+++ main.tf
@@ -83,3 +83,26 @@ resource "azurerm_linux_virtual_machine" "env1" {
     version   = "latest"
   }
 }
+
+
+
+###########################################
+#                   NSG                   #
+##########################################
+
+resource "azurerm_network_security_group" "env1" {
+  name                = "SSH-NSG"
+  location            = azurerm_resource_group.env1.location
+  resource_group_name = azurerm_resource_group.env1.name
+
+  security_rule {
+    name                       = "SSH22"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
