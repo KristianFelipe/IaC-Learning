7c922a1f77c15d7442eaee379e625c3e58371aaa
KristianFelipe <kristianlindgren3@gmail.com>
2025-03-25 10:00:10 +0100

    Added NSG to NIC

--- main.tf
+++ main.tf
@@ -91,19 +91,26 @@ resource "azurerm_linux_virtual_machine" "env1" {
 ##########################################
 
 resource "azurerm_network_security_group" "env1" {
-  name                = "SSH-NSG"
+  name                = "NSG-Allow-SSH"
   location            = azurerm_resource_group.env1.location
   resource_group_name = azurerm_resource_group.env1.name
 
   security_rule {
-    name                       = "SSH22"
+    name                       = "Allow-SSH"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
   }
+
+  resource "azurerm_network_interface_security_group_association" "env1" {
+  network_interface_id      = azurerm_network_interface.env1.id
+  network_security_group_id = azurerm_network_security_group.env1.id
+}
+
